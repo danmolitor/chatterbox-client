99416b23a6ac98f3d75da8daa5c857e48054de78
Dan Molitor <danmolitor@Piersons-MBP.home>
2016-01-26 19:39:51 -0800

    added the main lobby

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,112 +1,120 @@
-// YOUR CODE HERE:
-$(document).ready(function() {
-    var posts;
 
-    //for loop length of obj.results.length
-    var app = {
 
-        showMessages: function(obj) {
-            console.log("3 - Log: ", obj);
-            // d3.select('#main').selectAll('div')
-            //     .data(obj.results)
-            //     .enter()
-            //     .append('div')
-            //     .text(function(d, i) {
-            //         var textOrMessage = d.text || d.message;
-            //         var userCheck = d.username || "anonymous"
-            //         return userCheck + ": " + textOrMessage;
-            //     })
-
-            //Each method
-            _.each(obj.results, function(value, index, collection) {
-                var textOrMessage = value.text || value.message;
-                var userCheck = value.username || "anonymous"
-
-                var element = document.createElement('div');
-                element.innerText = userCheck + ": " + textOrMessage;
-                $('#main').append(element);
-
-            })
-        }
-    }
-
-    $.ajax({
-        // This is the url you should use to communicate with the parse API server.
-        url: 'https://api.parse.com/1/classes/chatterbox',
-        type: 'GET',
-        data: {
-            order: "-createdAt"
-        },
-        contentType: 'application/json',
-        success: function(data) {
-            console.log(' 2 - Data Received: ', data);
-            app.showMessages(data);
-            // posts = data.results;
-            // showMessages(posts)
-        },
-        error: function(data) {
-            // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-            console.error('chatterbox: Failed to send message. Error: ', data);
-        }
+var app = { };
+var messageIds = [];
+app.friends=[];
+app.roomNamesArray = [];
+app.roomname = 'lobby';
+//$('.messages').html("")
+function showMessages(results) {
+    
+    var roomResults = _.filter(results, function(element){
+        return element.roomname === app.roomname;
     })
 
+    _.each(roomResults,function(message){
+        var textOrMessage = message.text || message.message;
+        var userCheck = message.username || "anonymous";
+        var createdAt = message.createdAt.split("").slice(11,19).join("");
 
+        if (messageIds.indexOf(message.objectId) === -1){
+        var $msg = $("<h4>")
+            .text( userCheck + " @ " + createdAt + " : " + textOrMessage )
+            .css({ "margin" : "10px"})
 
-    setInterval(function() {
-
-
-
-        // console.log("1 - Document Ready ");
-        $.ajax({
-            // This is the url you should use to communicate with the parse API server.
-            url: 'https://api.parse.com/1/classes/chatterbox',
-            type: 'GET',
-            data: {
-                order: "-createdAt"
-            },
-            contentType: 'application/json',
-            success: function(data) {
-                console.log(' 2 - Data Received: ', data);
-                app.showMessages(data);
-                // posts = data.results;
-                // showMessages(posts)
-            },
-            error: function(data) {
-                // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-                console.error('chatterbox: Failed to send message. Error: ', data);
+        var $userTag = $('<br/><a "userName">---'+userCheck+'</a>')
+                .attr("class", userCheck)
+                .appendTo($msg) 
+                .css({ "color":"blue"})
+        if(app.friends.indexOf(userCheck) !== -1 ){
+            $userTag.css({"color":"red"})
+        }
+                
+        $($userTag).on("click", function(){
+            if(app.friends.indexOf(userCheck) === -1){
+                app.friends.push(userCheck);
             }
         })
 
+        var $container = $("<div>")
+            .attr("class","messages")
+            .css({ 
+                'background-color' : '#e5f2ff',
+                "margin" : "10px",
+                // "border" : "black solid 1px",
+                "border-radius" : "5px"
+             })
+            .prependTo(".beginning")
+            .append($msg)
+            messageIds.push(message.objectId);
+        }
+    })
+};
 
-    }, 20000)
+app.fetch = function(){
+    $.ajax({
+        url: 'https://api.parse.com/1/classes/chatterbox',
+        type: 'GET',
+        data: {order : "-createdAt" },
+        contentType: 'application/json',
+        success: function(data) {
+            console.log(' 2 - Data Received: ', data);
+            showMessages(data.results);
+            //process room data
+            app.populateRooms(data.results);
+            messages = data;
+        },
+        error: function(data) {
+            console.error('chatterbox: Failed to send message. Error: ', data);
+        }
+    });
+    setTimeout(app.fetch, 5000);
+}
 
+app.post = function(message){
+    $.ajax({
+        url: 'https://api.parse.com/1/classes/chatterbox',
+        type: 'POST',
+        data: JSON.stringify(message),
+        contentType: 'application/json',
+            success: function (data) {
+            console.log('chatterbox: Message sent. Data: ', data);
+        },
+        error: function (data) {
+            console.error('chatterbox: Failed to send message. Error: ', data);
+        }
+    });
+};
+
+app.populateRooms = function(results) {
+    app.$roomSelect.html('<option value="__newRoom">New room...</option><option value="" selected>Lobby</option></select>');
+
+};
+
+function init(){
+    $("#sendMessage").on("click",function(){
+        var message = {
+            username: window.location.search.split("").slice(10,19).join(""),
+            text: $(".userInput").val(),
+            roomname: '...'
+        };
+        // console.log(message);
+        app.post(message);
+    });
+    app.fetch();
+    var $placeHolder = $("<div>")
+        .attr("class","beginning")
+        .appendTo("#main")
+        .append($placeHolder);
+          // Cache jQuery selectors
+            app.$roomSelect = $('#roomSelect');
+            app.$roomSelect.on('change', function(){ app.roomname = app.$roomSelect.val(); });
+            // Add listeners
+            app.$roomSelect.on('change', app.saveRoom);
+}
+
+
+
+$(document).ready(function(){
+    init();
 })
-
-
-$('#sendMessage').on('click', function(){
-    console.log("hello")
-    userMessage.username = document.getElementById('.userInput').value;
-})
-
-
-// $.ajax({
-//   url: 'https://api.parse.com/1/classes/chatterbox',
-//   type: 'POST',
-//   data: JSON.stringify(message),
-//   contentType: 'application/json',
-//   success: function (data) {
-//     console.log('chatterbox: Message sent. Data: ', data);
-//   },
-//   error: function (data) {
-//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//     console.error('chatterbox: Failed to send message. Error: ', data);
-//   }
-// });
-
-// var message = {
-//   username: 'shawndrost',
-//   text: 'trololo',
-//   roomname: '4chan'
-// };
-
-var x = $(".userInput").val()
